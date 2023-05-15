fff71b8a4e543543234cd02204921a843608a2ad
Kris <kristopher52520@gmail.com>
2023-05-15 17:05:59 -0400

    Initialized Command Line Script.
    Title.

---
+++ LinuxCommandLine.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Commands
+    # ls -  Provides List of Contents in Current Directory.
+    # pwd - Prints Working Directory Path (Relative to Home Directory).
+    # cd - Changes Directory.
+    # touch - Creates a new file.
+    # file - Tells you what kind of file a file is (Since everything in Linux is a file).
+    # cat - Stands for "Concatenate". Simple command for displaying a files contents. Not great for viewing large files, good for short content.
+    # less - Better for viewing the output of larger files. 
+    # history - Provides a list of commands you already used. 
+    # clear - Clears the terminal. 
+    # cp - Used to copy a file. 
+    # mv - Used to move a file. Also used to move directories, rename a file, move more than one file, and rename directories.
+    # mkdir - Creates a new directory.
+    # rm - Removes a file. 
+    # rmdir - Removes a directory. 
+    # find - Lets you search for a specific file within a directory.
+    # help - Command that provides help for other bash commands. Gives you a description and command line arguements for the command you need help with. 
+    # man - Allows you to see the manual for the command you specify. Provides the full name, a synopsis for how to use it, a description of it's function, and command line arguements.
+    # whatis - Gives a short description of what a command does. 
+    # alias - Used for commands that get really repetitive to use. Can be created by the user. 
+    # exit - Used to exit the terminal.
