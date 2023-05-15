8818f2503981296ac38aee43f69acbee01ebf3b9
Kris <kristopher52520@gmail.com>
2023-05-15 16:33:11 -0400

    Created Script and README.md Files
    Title.

---
+++ LinuxDistribution.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This scripting file will be used to allow me to learn and review information for various Linux Distributions, from Ubuntu and Arch to Fedora and PopOs.
+
+# Choosing a Linux Distribution
+
+# Concepts To Learn: Rolling Release Method (Debian and Arch), Package Management Systems
+
+# Words To Learn: Incremental, Proprietary, Enterprise, Derivatives
+
+# Main Three Package Managers: 
+# DPKG - Debian Package Management System
+
+    # APT (Advanced Packaging Tool)
+    # APM (Aptitutde Packagae Manager)
+    # Synaptic Package Manager
+
+# RPM - Red Hat Package Manager
+    
+    # YUM (Yellowdog Updater, Modified)
+    # DNF (Dandified YUM)
+
+# Pacman Package Manager - Arch Linux
+
+# Definitions:
+    # Hardware - Includes all of the hardware that your system runs on as well as memory, CPU, disks, etc. 
+    # Linux Kernel - The core of the opearting system. This manages the hardware and tells it how to interact with the system. 
+    # For a better understanding, check out a graphic that demonstrates the connection between the operating system and the kernel. 
+    # User Space - This is where users like me directly interact with the system. 
+
+# Linux Distributions
+    # Debian - Distro composed entirely of free and open-source software. Consists of 3 branches, "Stable", "Testing", and "Unstable". Debian Package Manager
+    # Red Hat Enterprise Linux - Has strict rules to restrict free re-distribution, though it still provides source code for free. Uses RPM Package Manager.
+    # Ubuntu - Most popular linux distro. Releases its own DE manager "Unity" by default. Great for beginners. Offers ease of use and great UI experience. Debian PM.
+    # Fedora - Backed by Red Hat. Community driven containing open-source and free software. RHEL branches off of Fedora. Ubuntu equivalent backed by RHEL with RPM PM.
+    # Linux Mint - Based off of Ubuntu. Uses Ubuntu's Software Repositories, so the same packages are on both distributions. Debian PM. Beginner Friendly. 
+    # Gentoo - Ridiculous flexibility but at a literall price. Similiarly to Arch Linux, for advanced users. Uses it's own PM known as Portage. Portage is easy to maintain. 
+    # Arch Linux - Lightweight and flexible. Driven 100% by the community. Uses the Rolling Release Method. Uses it's own PM, Pacman. Great choice for hardcore Linux users, not beginners. 
+    # OpenSUSE - Created by the openSUSE Project. Second oldest still running Linux Distro. Purpose is to promote the use of Linux everywhere with free open source software. Uses RPM Package Manager.
+
+echo "Open script in Visual Studio Code for information regarding Linux Distributions"
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Linux Journey
+# 5/15/2023: Initialized script and added information. 
