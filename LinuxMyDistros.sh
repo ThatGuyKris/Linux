50fa5345d09721d2595c9d27fd470cb6a02f7f0d
Kris <kristopher52520@gmail.com>
2023-05-17 12:39:18 -0400

    Created LinuxMyDistros.sh and Added Executable Permissions.
    Title.

---
+++ LinuxMyDistros.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Current Distributions
+    # Fedora 38 Gnome (Laptop)
+    # Fedora (KDE Plasma Spin)
+
+# Distributions I am interested in:
+    # PopOs
+    # Arch Linux
+
+# Distributions I like but won't use right now: 
+    # Manjaro
+    # Gentoo
+    # Linux Mint
+
+# Distributions I do not like:
+    # Ubuntu
+
+# Distributions I will becoime familiar with: 
+    # Fedora (Main)
+    # Arch Linux (Backup 1)
+    # Linux Mint
+    # Manjaro (Backup 2)
+    # Gentoo
+
+echo "Script that contains information on my Linux Distribution preferences and plans."
