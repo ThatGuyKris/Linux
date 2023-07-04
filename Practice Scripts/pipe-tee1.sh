469640f680b144f46b137175e3ad03f4ab732a2c
Kris <kristopher52520@gmail.com>
2023-07-04 11:24:24 -0400

    Updated Piping Script
    Title.

--- Practice Scripts/pipe-tee1.sh
+++ Practice Scripts/pipe-tee1.sh
@@ -6,4 +6,4 @@ ls -la /etc | less
 # The tee command is used with a pipe, and writes the output of a program to standard output and simultaneously copies it into the specified file or files.
 ls | tee peanuts.txt
 
-
+# When we run this script, we can see that the output of the command ls is redirected to standard output, as well as copied to the specified file.
