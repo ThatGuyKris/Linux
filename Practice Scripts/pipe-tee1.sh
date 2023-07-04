c8e9d0ed614cf5b66f06affce162267ea79a9aaa
Kris <kristopher52520@gmail.com>
2023-07-04 11:19:10 -0400

    Created Script for Piping and Tee
    Title.

---
+++ Practice Scripts/pipe-tee1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Lots of info comes from this command. So, let's use the output of the command in another command, the less command.
+ls -la /etc | less
+
+# The tee command is used with a pipe, and writes the output of a program to standard output and simultaneously copies it into the specified file or files.
+ls | tee peanuts.txt
+
+
